deb41e1028a850711398e6b324530f5013b300f7
Avinash <avkr258@gmail.com>
2024-02-03 20:11:24 -0500

    Added lines to the PDF

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ for index, row in data.iterrows():
     pdf.set_font(family='Times', style='B', size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row['Topic'], align='L', ln=1)
-    pdf.line(10, 22, 200, 22)
+    for x in range(20,298,10):
+        pdf.line(10, x, 200, x)
 
     # Set footer
     pdf.ln(265)
@@ -29,5 +30,7 @@ for index, row in data.iterrows():
         pdf.set_font(family="Times", style="I", size=12)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=row["Topic"], align='R')
+        for x in range(20, 298, 10):
+            pdf.line(10, x, 200, x)
 
 pdf.output("output.pdf")
